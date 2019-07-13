5c262049f4f252063f4f4cf8a55dbd08b9fb5571
Khaotang <speeranat911@gmaii.com>
2019-07-13 15:13:27 +0700

    Add Score
    Add Score and Code For Tell a Score

---
+++ Exercise4_Peeranat_S.py
@@ -0,0 +1,10 @@
+ScoreEng = 99
+ScoreGB = 99
+ScoreICS  = 99
+ScoreCP = 99
+
+print("-------------Your Score-------------")
+print("Foundation English =",ScoreEng)
+print("General Business",ScoreGB)
+print("Introduction to Computer Systems",ScoreICS)
+print("Computer Programming",ScoreCP)
