5307d2131abe205479c3594c27f5d52b12c12aaa
Khaotang <speeranat911@gmaii.com>
2019-07-16 17:49:47 +0700

    เพิ่มเติมบางส่วน

--- Exercise5_1_Peeranat_S.py
+++ Exercise5_1_Peeranat_S.py
@@ -1,5 +1,5 @@
-x = int(input("Enter Your Number"))
-y = int(input("Enter Your Number"))
+x = int(input("Enter Your Number :"))
+y = int(input("Enter Your Number :"))
 print("x + y =",x+y)
 print("x - y =",x-y)
 print("x * y =",x*y)
